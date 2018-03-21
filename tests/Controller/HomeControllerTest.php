60b4de32bdd7c679f522ff611a7ba7681ece39f5
Kirill Rossokhin <rossokhin.kirov@gmail.com>
2018-03-21 23:12:13 +0300

    3/4 success

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -7,22 +7,37 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
 {
+    protected $client;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
 
     public function testIndex()
     {
-        $client = $this->createClient();
-        $client->request('GET', '/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $this->client->request('GET', '/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
 
     }
 
     public function testAddPost()
     {
-        $this->assertTrue(true);
+//        $client = $this->createClient();
+        $this->client->request('GET', '/add');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
     public function testPostSingle()
     {
         $this->assertTrue(true);
+        $this->client->request('GET', '/post/');
+        $this->assertEquals(404, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function tearDown()
+    {
+        $this->client = null;
     }
 }
