ee132144943b3c90fc28331453baeb7b642a708b
Kirill Rossokhin <rossokhin.kirov@gmail.com>
2018-03-21 09:41:25 +0300

    add post page form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends Controller
 {
@@ -10,6 +15,33 @@ class HomeController extends Controller
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    public function addPost(Request $request, PostRepository $postRepository) {
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post, [
+            'method' => 'POST'
+        ]);
+
+        $form->add('submit', SubmitType::class, [
+            'label' => 'Add post'
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('add-post/index.html.twig', [
+            'controller_name' => 'HomeController:addPost',
+            'form' => $form->createView(),
         ]);
     }
 }
