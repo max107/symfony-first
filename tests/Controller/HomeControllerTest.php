a1384d623b52cba154335716e1f737bcbd0703a8
Kirill Rossokhin <rossokhin.kirov@gmail.com>
2018-03-21 14:56:01 +0300

    знакомство с phpunit

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class HomeControllerTest extends TestCase
+{
+
+    public function testIndex()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testAddPost()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testPostSingle()
+    {
+        $this->assertTrue(true);
+    }
+}
